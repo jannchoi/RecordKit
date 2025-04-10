982023daf244432a66a0e492d5d7a55adac88d5c
jannchoi <gongjo3695@gmail.com>
2025-04-10 18:02:11 +0900

    feat: RecordUseCase factory method 추가

--- Sources/RecordKit/Interface/RecordKit.swift
+++ Sources/RecordKit/Interface/RecordKit.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+public enum RecordKit {
+    public static func makeUseCase() -> RecordUseCaseProtocol {
+        let mapper = RecordMapper()
+        let dataSource = RecordDataSource(mapper: mapper)
+        let repository = RecordRepository(dataSource: dataSource, mapper: mapper)
+        let useCase = RecordUseCase(repository: repository)
+        return useCase
+    }
+    public static func makeUseCase(repository: RecordRepositoryProtocol) -> RecordUseCaseProtocol {
+        return RecordUseCase(repository: repository)
+    }
+
+}
